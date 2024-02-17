3c4da9331333b2ab1aabe51a3d49c67b83d4d35c
Mihir Chauhan <mihirchauhan951@gmail.com>
2024-02-17 09:48:20 +0000

    Refactor authentication endpoint and update request payload

--- Nodejs-ReactJs_Chatapp/frontend/src/AuthPage.jsx
+++ Nodejs-ReactJs_Chatapp/frontend/src/AuthPage.jsx
@@ -1,13 +1,8 @@
-import axios from "axios";
-
 const AuthPage = (props) => {
   const onSubmit = (e) => {
     e.preventDefault();
     const { value } = e.target[0];
-    axios
-      .post("http://localhost:3001/authenticate", { username: value })
-      .then((r) => props.onAuth({ ...r.data, secret: value }))
-      .catch((e) => console.log("Auth Error", e));
+    props.onAuth({ username: value, secret: value });
   };
 
   return (
